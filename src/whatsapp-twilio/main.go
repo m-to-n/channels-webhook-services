13205c0dfa07c8294e6af12a9b0794bf9af7aef6
Adam Bezecny <adam.bezecny@gmail.com>
2022-02-25 17:58:25 +0100

    message fixes

--- src/whatsapp-twilio/main.go
+++ src/whatsapp-twilio/main.go
@@ -20,7 +20,7 @@ func Handler(ctx context.Context) (Response, error) {
 	var buf bytes.Buffer
 
 	body, err := json.Marshal(map[string]interface{}{
-		"message": "Future WhatsApp webhook handler for Twilio provider running here!",
+		"message": "Future WhatsApp webhook handler for Twilio provider running here!!!",
 	})
 	if err != nil {
 		return Response{StatusCode: 404}, err
